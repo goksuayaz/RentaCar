9117ba1f399b0e6fbb926e8f606d83abe6557ba0
Göksu Ayaz <goksuayaz79@gmail.com>
2024-04-02 02:15:47 +0300

    Initial commit

---
+++ src/view/AdminView.java
@@ -0,0 +1,109 @@
+package view;
+
+import business.BrandManager;
+import core.Helper;
+import entity.Brand;
+import entity.User;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+
+public class AdminView extends Layout{
+    private JPanel container;
+    private JLabel lbl_welcome;
+    private JPanel pnl_top;
+    private JTabbedPane tab_menu;
+    private JButton btn_logout;
+    private JPanel pnl_brand;
+    private JScrollPane scl_brand;
+    private JScrollPane scrl_brand;
+    private JTable tbl_brand;
+    private User user;
+    private DefaultTableModel tmdl_brand = new DefaultTableModel();
+    private BrandManager brandManager;
+    private JPopupMenu brandMenu;
+    public AdminView(User user){
+        this.brandManager = new BrandManager();
+        this.add(container);
+        this.guiInitilaze(1000,500);
+        this.user = user;
+
+        if(this.user == null){
+            dispose();
+        }
+
+        this.lbl_welcome.setText("Hoşgeldiniz : " + this.user.getUsername());
+
+        loadBrandTable();
+        loadBrandComponent();
+
+
+
+
+        this.tbl_brand.setComponentPopupMenu(brandMenu);
+
+
+    }
+
+    public void loadBrandComponent(){
+        this.tbl_brand.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int selected_row = tbl_brand.rowAtPoint(e.getPoint());
+                tbl_brand.setRowSelectionInterval(selected_row, selected_row);
+
+            }
+
+        });
+        this.brandMenu = new JPopupMenu();
+        this.brandMenu.add("Yeni").addActionListener(e ->{
+            BrandView brandView = new BrandView(null);
+            brandView.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosed(WindowEvent e) {
+                    loadBrandTable();
+                }
+            });
+
+        });
+        this.brandMenu.add("Güncelle").addActionListener(e -> {
+            int selectBrandId = this.getTableSelectedRow(tbl_brand, 0);
+            BrandView brandView = new BrandView(this.brandManager.getById(selectBrandId));
+            brandView.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosed(WindowEvent e) {
+                    loadBrandTable();
+                }
+            });
+        });
+
+        this.brandMenu.add("Sil").addActionListener(e -> {
+            if(Helper.confirm("sure")){
+            int selectBrandId = this.getTableSelectedRow(tbl_brand,0);
+                if(this.brandManager.delete(selectBrandId)){
+                    Helper.showMsg("done");
+                    loadBrandTable();
+                }else{
+                    Helper.showMsg("error");
+                }
+            }
+
+        });
+
+
+    }
+
+
+    public void loadBrandTable(){
+
+        Object[] col_brand = {"Marka ID", "Marka Adı"};
+        ArrayList<Object[]> brandList = this.brandManager.getForTable(col_brand.length);
+        this.createTable(this.tmdl_brand, this.tbl_brand, col_brand, brandList);
+
+    }
+}
